f622bf396892aac085abff407bc38514c166eab3
João Mathias Pereira Rezende <joao.mathias@audaces.com>
2018-09-24 14:21:41 -0300

    citizen addition

---
+++ citizen.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+
+typedef enum { HIGHER = 0, MEDIUM = 1, LOWER = 2 } PRIORITY;
+
+typedef struct Citizen {
+    PRIORITY priority;
+    int id;
+} Citizen;
+
+Citizen* makeCitizen(PRIORITY priority, int id)
+{
+    Citizen* citizen = malloc(sizeof(Citizen));
+
+    citizen->priority= priority;
+    citizen->id = id;
+
+    return citizen;
+}
+
+void change_priority(Citizen* citizen, PRIORITY priority)
+{
+    citizen->priority = priority;
+}
--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include "citizen.c"
 
 int main()
 {
-    printf("Hello world!");
+    Citizen* citizen = makeCitizen(LOWER, 1);
+
+    change_priority(citizen, MEDIUM);
+
 	return 0;
 }
